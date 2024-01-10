c3f5cadd97b5303c5297a959f8d4aec4357b1451
tenkiame6 <koga.mizuki750@mail.kyutech.jp>
2024-01-10 17:34:24 +0900

    [Add] 各IWPにinterestが来た数とキャッシュヒットした数を配列に保存する

--- forwarder.cpp
+++ forwarder.cpp
@@ -348,7 +348,7 @@ Forwarder::onIncomingData(const Data& data, const FaceEndpoint& ingress)
 
 
   int node = ns3::Simulator::GetContext();
-  float p1 = 0.1, p2 = 0.1, p3 = 0.1, p4 = 0.1;//各ノードのキャッシュ確率→グローバルに
+  float p1 = 0.1, p2 = 0.1, p3 = 0.1, p4 = 0.1;//各ノードのキャッシュ確率→あとでグローバルに
   //ここでhit率に基づいてpの更新 hit率をどうとるか？
   
   //ここで一定時間を作る
